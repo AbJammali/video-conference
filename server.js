f1b6c8f3bd2688ebda82009e270dd27df1f43499
AbJammali <101871589+AbJammali@users.noreply.github.com>
2025-08-05 14:32:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,29 +7,54 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route all /room/* URLs to index.html
+// Serve index.html for room links
 app.get('/room/:room', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// WebSocket handling
-io.on('connection', socket => {
-  socket.on('join', room => {
-    socket.join(room);
-    socket.to(room).emit('user-connected', socket.id);
+const rooms = {};
 
-    socket.on('signal', data => {
-      socket.to(room).emit('signal', data);
+io.on('connection', socket => {
+  socket.on('join', roomId => {
+    socket.join(roomId);
+
+    // Track users in the room
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(socket.id);
+
+    // Send list of users to new user
+    const otherUsers = rooms[roomId].filter(id => id !== socket.id);
+    socket.emit('users', otherUsers);
+
+    // Notify existing users
+    socket.to(roomId).emit('user-connected', socket.id);
+
+    // Relay signaling messages
+    socket.on('offer', ({ target, sdp }) => {
+      io.to(target).emit('offer', { sender: socket.id, sdp });
+    });
+
+    socket.on('answer', ({ target, sdp }) => {
+      io.to(target).emit('answer', { sender: socket.id, sdp });
+    });
+
+    socket.on('ice-candidate', ({ target, candidate }) => {
+      io.to(target).emit('ice-candidate', { sender: socket.id, candidate });
     });
 
     socket.on('disconnect', () => {
-      socket.to(room).emit('user-disconnected', socket.id);
+      // Remove user from room
+      for (const room in rooms) {
+        rooms[room] = rooms[room].filter(id => id !== socket.id);
+        socket.to(room).emit('user-disconnected', socket.id);
+      }
     });
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
